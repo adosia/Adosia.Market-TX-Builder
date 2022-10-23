8719dbc4e40cbf70c03372677a9f71e5955d5f3c
Latheesan Kanesamoorthy <latheesan_k@hotmail.com>
2022-10-23 19:28:27 +0100

    main Solve cardano node db issue in docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,12 +2,10 @@
 
 set -m
 
-sudo chmod 777 -R ${HOME}/cardano-node/db &&
-
 ${HOME}/cardano-node/bin/cardano-node run \
   --topology ${HOME}/cardano-node/config/topology.json \
-  --database-path ${HOME}/cardano-node/db \
-  --socket-path ${HOME}/cardano-node/db/node.socket \
+  --database-path ${HOME}/cardano-node-data/db \
+  --socket-path ${HOME}/cardano-node-data/db/node.socket \
   --host-addr 0.0.0.0 \
   --port 6000 \
   --config ${HOME}/cardano-node/config/config.json &
